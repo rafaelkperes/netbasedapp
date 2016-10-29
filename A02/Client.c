00465d90298aa2a1c4feaf735c4ced42479c996b
danish-answers <danish.answers@gmail.com>
2016-10-29 14:12:02 +0200

    working code

--- A02/Client.c
+++ A02/Client.c
@@ -1,5 +1,3 @@
-/************* UDP CLIENT CODE *******************/
-
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,7 +8,6 @@ void tcp_server(){
     struct sockaddr_in serv_addr_tcp;
 
     char recvBuff[1025];
-    time_t ticks;
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr_tcp, '0', sizeof(serv_addr_tcp));
@@ -23,7 +20,6 @@ void tcp_server(){
     bind(listenfd, (struct sockaddr*)&serv_addr_tcp, sizeof(serv_addr_tcp));
 
     listen(listenfd, 10);
-
 
 	connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
 
@@ -41,11 +37,9 @@ int main(){
 	struct sockaddr_in serv_addr_tcp;
 
 	char recvBuff[1025];
-	time_t ticks;
 
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 	memset(&serv_addr_tcp, '0', sizeof(serv_addr_tcp));
-	//memset(recvBuff, '0', sizeof(recvBuff));
 
 	serv_addr_tcp.sin_family = AF_INET;
 	serv_addr_tcp.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -64,46 +58,42 @@ int main(){
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
 
-  /*Create UDP socket*/
   clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
 
-  /*Configure settings in address struct*/
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_port = htons(3000);
   serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  /*Initialize size variable to be used later on*/
   addr_size = sizeof serverAddr;
 
 
-    printf("Type a sentence to send to server:\n");
-    fgets(buffer,1024,stdin);
-    printf("You typed: %s",buffer);
+	printf("Type the name of the file to be requested (e.g. \"sugar.txt\"):\n");
+	fgets(buffer,1024,stdin);
+	printf("File requested: \"%s\"",buffer);
 
-    nBytes = strlen(buffer) + 1;
+	nBytes = strlen(buffer) + 1;
 
-    /*Send message to server*/
-    sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
+	/*Send message to server*/
+	sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
 
-    /*Receive message from server*/
-                nBytes = recvfrom(clientSocket,buffer,1024,0,NULL, NULL);
-
-    printf("Received from server: %s\n",buffer);
-
+	/*Receive message from server*/
+				nBytes = recvfrom(clientSocket,buffer,1024,0,NULL, NULL);
 
 
     //////////////////////////server listen//////////////////////
 	connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
 	read(connfd, recvBuff, sizeof(recvBuff)-1);
-	printf("server: %s\n",recvBuff);
 	close(connfd);
 	/////////////////////////////////////////////////////////////
 
 	////////////read into file
+	//printf("Received from server: %s\n",recvBuff);
 	FILE *fp;
 	fp=fopen("received_file.txt","w");
 	fprintf(fp,"%s",recvBuff);
+	printf("Received file over TCP, PORT 2000\n");
+	printf("File saved as \"received_file.txt\"\n");
 	//////////////////////////
 
   return 0;
--- A02/Server.c
+++ A02/Server.c
@@ -1,17 +1,8 @@
-/************* UDP SERVER CODE *******************/
-
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <stdlib.h>
-
-int sendFileToClient(char buffer[]);
-
-int sendFileToClient(char buffer[]){
-	printf("server: %s\n",buffer);
-	return 1;
-}
 
 int tcp_client(char buffer[]){
 	sleep(1);
@@ -20,7 +11,6 @@ int tcp_client(char buffer[]){
 
     struct sockaddr_in serv_addr;
 
-    //memset(sendBuff, '0',sizeof(sendBuff));
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Error : Could not create socket \n");
@@ -45,10 +35,13 @@ int tcp_client(char buffer[]){
     }
 
     ///////////file operation
-    FILE *f = fopen("add.txt", "rb");
+    char filename[strlen(buffer)];
+    strcpy(filename, buffer);
+    strtok(filename, "\n");
+    FILE *f = fopen(filename, "rb");
     fseek(f, 0, SEEK_END);
     long fsize = ftell(f);
-    fseek(f, 0, SEEK_SET);  //same as rewind(f);
+    fseek(f, 0, SEEK_SET);
 
     char *string = malloc(fsize + 1);
     fread(string, fsize, 1, f);
@@ -57,6 +50,7 @@ int tcp_client(char buffer[]){
     string[fsize] = 0;
     /////////////////////////
     write(sockfd, string, strlen(string));
+    printf("File sent.\n");
     close(sockfd);
     return 0;
 }
@@ -70,30 +64,24 @@ int main(){
   socklen_t addr_size, client_addr_size;
   int i;
 
-  /*Create UDP socket*/
   udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
 
-  /*Configure settings in address struct*/
   serverAddr_udp.sin_family = AF_INET;
   serverAddr_udp.sin_port = htons(3000);
   serverAddr_udp.sin_addr.s_addr = inet_addr("127.0.0.1");
   memset(serverAddr_udp.sin_zero, '\0', sizeof serverAddr_udp.sin_zero);
 
-  /*Bind socket with address struct*/
   bind(udpSocket, (struct sockaddr *) &serverAddr_udp, sizeof(serverAddr_udp));
 
-  /*Initialize size variable to be used later on*/
   addr_size = sizeof serverStorage;
 
   while(1){
-    /* Try to receive any incoming UDP datagram. Address and port of
-      requesting client will be stored on serverStorage variable */
-    nBytes = recvfrom(udpSocket,buffer,1024,0,(struct sockaddr *)&serverStorage, &addr_size);
+	printf("UDP Server Running on PORT: 3000 ...\n");
+	nBytes = recvfrom(udpSocket,buffer,1024,0,(struct sockaddr *)&serverStorage, &addr_size);
 
-    /*Send uppercase message back to client, using serverStorage as the address*/
-    sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
-    //sent_udp = sendFileToClient(buffer);
-    tcp_client(buffer);
+	sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
+
+	tcp_client(buffer);
   }
 
   return 0;
