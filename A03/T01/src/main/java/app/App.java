8ed979cc123eba95d7cda87b62a8348378bdc1c9
rafaelkperes <rafaelkperes@gmail.com>
2016-11-09 11:27:22 +0100

    Parse args, parse URL and connect to TCP socket
    Start NetBeans project over generated Maven project. Parse URL out of args
    and use java.net.URL to parse URL parameters. Use URL host and port to
    open connection on connect(). Create tests for connect().

--- A03/T01/src/main/java/app/App.java
+++ A03/T01/src/main/java/app/App.java
@@ -1,13 +1,68 @@
 package app;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.Socket;
+import java.net.URL;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * Hello world!
+ * HTTP/1.1
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static final int DEFAULT_PORT = 80;
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
+    static Socket connect(String host, int port) throws IOException {
+        LOGGER.log(Level.FINE, "Connecting to: {0}:{1}",
+                new Object[]{host, port});
+        Socket clientSocket = new Socket(host, port);
+
+        return clientSocket;
     }
+
+    static void start(String[] args) throws Exception {
+        LOGGER.log(Level.FINER, "Start called with args: {0}", args);
+
+        if (args.length < 1) {
+            throw new Exception("I need an URL to GET.");
+        } else {
+            URL url = new URL(args[0]);
+            LOGGER.log(Level.FINER, "Parsed HOST:", url.getHost());
+            LOGGER.log(Level.FINER, "Parsed PORT:", url.getPort());
+
+            /* if URL does not contain PORT, set default value */
+            int port = url.getPort();
+            if (port == -1) {
+                port = DEFAULT_PORT;
+            }
+
+            Socket sock = connect(url.getHost(), port);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Handler fh = new FileHandler("./all.log");
+        LOGGER.addHandler(fh);
+        LOGGER.setLevel(Level.FINEST);
+        try {
+            LOGGER.log(Level.FINE, "Main called with args: {0}", args);
+            start(args);
+        } catch (Exception e) {
+            System.out.println("Execution returned an exception:");
+            System.out.println(e.getMessage());
+
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            LOGGER.log(Level.SEVERE, sw.toString());
+        }
+    }
+
 }
--- A03/T01/src/test/java/app/AppTest.java
+++ A03/T01/src/test/java/app/AppTest.java
@@ -1,38 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static app.App.connect;
+import java.io.IOException;
+import static junit.framework.TestCase.fail;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    public AppTest() {
     }
 
     /**
-     * @return the suite of tests being tested
+     * Test connect() call with valid and invalid URLs (Internet connection
+     * needed).
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testConnect() {
+        System.out.println("connect");
+
+        try {
+            connect("www.google.com", 80);
+
+        } catch (IOException e) {
+            fail("Shouldn't throw exception for invalid host.");
+        }
+
+        try {
+            connect("www.google.com", 1);
+            fail("Should throw exception for invalid port.");
+        } catch (IOException e) {
+        }
+
+        try {
+            connect("asdfasf", 80);
+            fail("Should throw exception for invalid host.");
+        } catch (IOException e) {
+        }
     }
 
     /**
-     * Rigourous Test :-)
+     * Test start() call failing without arguments
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testStart() {
+        System.out.println("start");
+
+        String[] args = null;
+        try {
+            App.start(args);
+            fail("Shouldn't work with null arguments!");
+        } catch (Exception e) {
+        }
+
+        args = new String[0];
+        try {
+            App.start(args);
+            fail("Shouldn't work without arguments!");
+        } catch (Exception e) {
+        }
+
     }
+
 }
