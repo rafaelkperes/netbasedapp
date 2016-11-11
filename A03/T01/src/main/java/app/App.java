d74f43a1e6685aa39f7520ab60aeda55dc4822f2
danish-answers <danish.answers@gmail.com>
2016-11-11 18:52:31 +0100

    open testtttt.jpg in a text editor. delete http response header and save the file. open by double clicking it and the image will be openend correctly.
    please write code to delete these lines regarding http response header

--- A03/T01/src/main/java/app/App.java
+++ A03/T01/src/main/java/app/App.java
@@ -60,22 +60,12 @@ public class App {
 			OutputStream dos = new FileOutputStream("RESULTS\\testtttt.jpg");
 			int count;
 			byte[] buffer = new byte[2048];
-			boolean eohFound = false;
-			while ((count = in.read(buffer)) != -1) {
-				if (!eohFound) {
-					String string = new String(buffer, 0, count);
-					int indexOfEOH = string.indexOf("\r\n\r\n");
-					if (indexOfEOH != -1) {
-						count = count - indexOfEOH - 4;
-						buffer = string.substring(indexOfEOH + 4).getBytes();
-						eohFound = true;
-					} else {
-						count = 0;
-					}
-				}
-				dos.write(buffer, 0, count);
-				dos.flush();
-			}
+		    while ((count = in.read(buffer)) != -1)
+		    {
+		      dos.write(buffer, 0, count);
+		      dos.flush();
+		    }
+		    dos.close();
 			// in.close();
 			dos.close();
 			System.out.println("image transfer done");
