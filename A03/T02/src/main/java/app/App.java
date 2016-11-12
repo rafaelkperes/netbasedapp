82aca62485145594c7c58517be32a514b8edef95
danish-answers <danish.answers@gmail.com>
2016-11-12 12:56:30 +0100

    working server implementation. need to implement functions for processing the request and serving the right file out of the root to the client.

--- A03/T02/src/main/java/app/App.java
+++ A03/T02/src/main/java/app/App.java
@@ -1,10 +1,28 @@
 package app;
 
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String data = "danish speaking from server";
+		while (true) {
+			try {
+				ServerSocket srvr = new ServerSocket(1234);
+				Socket skt = srvr.accept();
+				System.out.print("Server has connected!\n");
+				PrintWriter out = new PrintWriter(skt.getOutputStream(), true);
+				System.out.print("Sending string: '" + data + "'\n");
+				out.print(data);
+				out.close();
+				skt.close();
+				srvr.close();
+			} catch (Exception e) {
+				System.out.print("Whoops! It didn't work!\n");
+			}
+		}
 	}
-
 }
