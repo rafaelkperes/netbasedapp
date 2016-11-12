41f23567e8b6f015ed92aa0e835b460634b6562a
danish-answers <danish.answers@gmail.com>
2016-11-12 20:41:53 +0100

    organized in a funciton

--- A03/T02/src/main/java/app/App.java
+++ A03/T02/src/main/java/app/App.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
@@ -13,13 +14,23 @@ import java.net.Socket;
 
 public class App {
 
-	public static void sendHTML(Socket skt){
-		
+	public static void sendHTML(File file, Socket skt) throws IOException{
+		String data = "HTTP/1.1 200 OK\nDate: Sat, 28 Nov 2009 04:36:25 GMT";
+		PrintWriter out = new PrintWriter(skt.getOutputStream(), true);
+		//out.print(data);
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String line;
+		out.println(data);
+		out.print("\n");
+	    while ((line = br.readLine()) != null) {
+	        // process the line.
+	    	out.print(line);
+	     }
+		out.close();
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String data = "HTTP/1.1 200 OK\nDate: Sat, 28 Nov 2009 04:36:25 GMT";
 		while (true) {
 			try {
 				File file = new File("dummy.html");
@@ -27,18 +38,7 @@ public class App {
 				ServerSocket srvr = new ServerSocket(1234);
 				Socket skt = srvr.accept();
 				System.out.print("Server has connected!\n");
-				PrintWriter out = new PrintWriter(skt.getOutputStream(), true);
-				//out.print(data);
-				BufferedReader br = new BufferedReader(new FileReader(file));
-				String line;
-				out.println(data);
-				out.print("\n");
-			    while ((line = br.readLine()) != null) {
-			        // process the line.
-			    	out.print(line);
-			     }
-				int count;
-				out.close();
+				sendHTML(file, skt);
 				skt.close();
 				srvr.close();
 			} catch (Exception e) {
