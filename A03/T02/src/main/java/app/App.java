d602b23b17e3f8214bd22504e14b186b53df4bd4
danish-answers <danish.answers@gmail.com>
2016-11-12 20:39:12 +0100

    html sent correctly with http response http header

--- A03/T02/src/main/java/app/App.java
+++ A03/T02/src/main/java/app/App.java
@@ -1,8 +1,10 @@
 package app;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
@@ -11,9 +13,13 @@ import java.net.Socket;
 
 public class App {
 
+	public static void sendHTML(Socket skt){
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String data = "danish speaking from server";
+		String data = "HTTP/1.1 200 OK\nDate: Sat, 28 Nov 2009 04:36:25 GMT";
 		while (true) {
 			try {
 				File file = new File("dummy.html");
@@ -21,9 +27,16 @@ public class App {
 				ServerSocket srvr = new ServerSocket(1234);
 				Socket skt = srvr.accept();
 				System.out.print("Server has connected!\n");
-				OutputStream out = skt.getOutputStream();
-				//PrintWriter out = new PrintWriter(skt.getOutputStream(), true);
+				PrintWriter out = new PrintWriter(skt.getOutputStream(), true);
 				//out.print(data);
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				String line;
+				out.println(data);
+				out.print("\n");
+			    while ((line = br.readLine()) != null) {
+			        // process the line.
+			    	out.print(line);
+			     }
 				int count;
 				out.close();
 				skt.close();
